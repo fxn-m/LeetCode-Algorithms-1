49e660efd705023f02b56b129bef7612f65a021b
Felix Newport-Mangell <felixnewportmangell98@gmail.com>
2022-05-17 01:43:56 +0100

    day 1 finished

---
+++ searchInsert.py
@@ -0,0 +1,19 @@
+class Solution:
+    def searchInsert(self, nums: list, target: int) -> int:
+        
+        l, r = 0, len(nums)-1
+        
+        while l <= r:
+            
+            mid = (l + r) // 2
+            
+            if nums[mid] == target:
+                return mid
+            
+            elif target < nums[mid]:
+                r = mid-1
+                
+            else:
+                l = mid+1                   
+                
+        return l
